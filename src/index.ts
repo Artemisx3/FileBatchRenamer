14ddb4df86fbd1f6a90c622af38e1037e626b160
Artemisx3 <misch.boon@gmail.com>
2024-09-03 15:40:48 +0200

    Feat: Added customizable file extention support, colors, more robust file sorting, file extention checking. Fix: fixed the order of the array when renaming the same files multiple times. fixed Enter to go back to menu

--- src/index.ts
+++ src/index.ts
@@ -2,67 +2,62 @@ import * as readline from 'readline'
 import * as fs from 'node:fs';
 import { rename } from 'node:fs';
 import * as path from 'path';
-
-
+let pc = require('picocolors')
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout});
+    output: process.stdout
+});
 
 function menu() {
-    console.log("1. Rename files in certain directory to")
-    console.log("2. Exit")
-}
+    console.log(pc.cyan("1. Rename files in certain directory to"))
+    console.log(pc.red("2. Exit"))}
 
 function handleChoiceMenu ()  {
-    rl.question("Enter your choice: ", (choice) => {
-    switch(choice){ 
+    rl.question(pc.cyan("Enter your choice: "), (choice) => {
+        switch(choice){ 
         case '1':
         console.clear()
-        rl.question("Enter the directory: ", (directory) => {
+        rl.question(pc.cyan("Enter the directory: "), (directory) => {
         renameFiles(directory)
         })
         break;
         case '2':
         process.exit();
-        break;
-    }  
+        }
     })
-    
 }
 
-let i: number = -1
 async function renameFiles(directory: string) {
-const files = fs.readdir(directory, (err, files) => {
-    if (err) throw err;
+const files = fs.readdirSync(directory)
+files.sort((a,b) => a.localeCompare(b,undefined, {numeric: true}));
     console.log('\n')
-    console.log('There are a total of ' + files.length + ' pictures in this directory\n')
-    rl.question('Enter the name of the files: \n', (name) => {
-    files.forEach(file => { 
-        i++
-        //this is the old path to the file we are renaming
-        let picture = directory + '/' + file 
-        //this is the new path where that file will be, the i is used to make each file unique and so it doesn't overwrite the old one
-        let newname = directory + '/' + name + '-' + i + '.png'
-        if (path.extname(file) === ".png"){
-            console.log(`renamed ${files[i]} to -> ${name}-${i}.png`);
-        rename(picture, newname, (err) =>    
-        {if (err) throw err; })
-        } 
-        else if (path.extname(file) !== ".png"){
-            console.log(`Skipped ${files[i]} because it is not a png!`);
-        }
-        
-        
-});
-})
-})
-console.log("\nPress Enter to go back to main menu")
-rl.on('line', (input) => {
-  if (input.trim() === '') {
-    main();
-  }
-});
+    console.log(pc.cyan('There are a total of ' + files.length + ' files in this directory\n'))
+    rl.question(pc.cyan("Enter the extension name of the files you'd like to change(e.g. jpg, png, etc.): \n"), (ext) => {
+        rl.question('Enter the new name for the files: \n', (name) => {
+            let i = 0
+            files.forEach((file) => { 
+                let fileName = path.join(directory, file)
+                let  newName = path.join(directory, `${name}-${i}.${ext.toLowerCase()}`)
+                if (path.extname(file) === '.' + ext.toLowerCase()) {
+                    console.log(pc.green(`Renamed ${file} to -> ${name}-${i}.${ext.toLowerCase()}`));
+                    rename(fileName, newName, (err) => {
+                        if (err) throw err;
+                    });
+                    i++
+                    } 
+                    else {
+                    console.log(pc.red(`Skipped ${file} because it is not a .${ext}`));
+                    }
+            });
+            console.log(pc.cyan("\nPress Enter to go back to main menu"))
+            rl.once('line', (input) => {
+                if (input.trim() === '') {
+                main();
+                }
+            });
+        });
+    });
 }
 
 function main() {
